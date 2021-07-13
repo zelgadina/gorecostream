4a2b923355755ea67d33d1b156c2f119a943e07b
zelgadina <zelgadina@yandex.ru>
2021-07-13 22:54:57 +0300

    Add files via upload
    MVP without unicode

--- gorecostream.go
+++ gorecostream.go
@@ -1,15 +1,3 @@
-// {"url": "http://ura-povara.ru/journal/6-produktov-kotorye-mogut-navredit-zhelchnomu-puzyrju", "state": "checked", "categories": ["good_site"], "category_another": "", "for_main_page": false, "ctime": 1567713280}
-
-// В поле categories указано например, good_site
-// Надо его распарсить и обкачать урлы из этого семпла. И сделать для каждой категории текстовый файл, в формате tsv, в котором должен лежать url\ttitle\tdescription
-
-// Пример, файл good_site.tsv
-// http://ura-povara.ru/journal/6-produktov-kotorye-mogut-navredit-zhelchnomu-puzyrju  6 продуктов, которые могут навредить желчному пузырю - Ура! Повара  И что есть, чтобы снизить риск воспалений в желчном?
-// Парсить надо максимально быстро, с минимумом ресурсов, но так, чтобы не забить канал/не положить сервер. Будет плюсом решение, не используещее внешних библиотек.
-
-
-// починить юникод
-
 package main
 
 import (
@@ -23,6 +11,7 @@ import (
     "time"
     "io"
     "golang.org/x/net/html"
+    "sync"
     // "unicode/utf8"
 )
 
@@ -33,152 +22,9 @@ type Doc struct {
     Description string
 }
 
-type Writer struct {
-    Channel     chan *Doc
-    File        *os.File
-}
+var categories = make(map[string]chan []string)
 
-var categories = make(map[string]*Writer)
-
-func createTSV(category string) (file *os.File) {
-    file, err := os.Create(fmt.Sprintf("%s.tsv", category))
-    checkError("Cannot create file ", err)
-    // defer file.Close()
-    return
-}
-
-func writeToTSV(file *os.File, write chan *Doc) {
-    writer := csv.NewWriter(file)
-    writer.Comma = '\t'
-    defer writer.Flush()
-    for {
-        doc, ok := <- write
-        if !ok {
-            close(write)
-            file.Close()
-            return
-        }
-        line := []string{doc.Url, doc.Title, doc.Description}
-        writer.Write(line)
-        if err := writer.Error(); err != nil {
-            log.Fatal("Error writing csv: ", err)
-        }
-    }
-}
-
-func selectCategory(snippets chan *Doc) {
-    for {
-        doc, ok := <-snippets
-        if !ok {
-            return
-        }
-        for _, category := range doc.Categories {
-            if _, ok := categories[category]; !ok {
-                categories[category] = new(Writer)
-                categories[category].Channel = make(chan *Doc)
-                categories[category].File = createTSV(category)
-                go writeToTSV(categories[category].File, categories[category].Channel)
-            }
-            categories[category].Channel <- doc
-        }
-    }
-}
-
-func checkError(message string, err error) {
-    if err != nil {
-        log.Fatal(message, err)
-    }
-}
-
-func getSnippet(urls, snippets chan *Doc) {
-    for {
-        doc, ok := <- urls
-        if !ok {
-            close(snippets)
-            return
-        }
-        client := &http.Client {
-            Timeout: 30 * time.Second,
-        }
-        resp, err := client.Get(doc.Url)
-
-        if err != nil {
-            log.Println(err)
-            continue
-        }
-        defer resp.Body.Close()
-
-        if resp.StatusCode == 404 {
-            log.Println(err)
-            continue
-        }
-
-        doc.Title, doc.Description = extract(resp.Body)
-        
-        snippets <- doc
-    }
-}
-
-func extract(resp io.Reader) (title, description string) {
-    z := html.NewTokenizer(resp)
-    titleFound := false
-    for {
-        tt := z.Next()
-        switch tt {
-        case html.ErrorToken:
-            return
-        case html.StartTagToken, html.SelfClosingTagToken:
-            t := z.Token()
-            if t.Data == "title" {
-                titleFound = true
-            }
-            if t.Data == "meta" {
-                desc, ok := extractMetaProperty(t, "description")
-                if ok {
-                    description = desc
-                }
-
-                ogTitle, ok := extractMetaProperty(t, "og:title")
-                if ok {
-                    title = ogTitle
-                }
-
-                ogDesc, ok := extractMetaProperty(t, "og:description")
-                if ok {
-                    description = ogDesc
-                }
-            }
-        case html.TextToken:
-            if titleFound {
-                t := z.Token()
-                title = t.Data
-                titleFound = false
-            }
-        }
-    }
-    return
-}
-
-func extractMetaProperty(t html.Token, prop string) (content string, ok bool) {
-    for _, attr := range t.Attr {
-        if attr.Key == "property" && attr.Val == prop {
-            ok = true
-        }
-
-        if attr.Key == "content" {
-            content = attr.Val
-        }
-    }
-    return
-}
-
-func printSnippet(snippets chan *Doc) {
-    for snippet := range snippets {
-        fmt.Println(snippet.Url, snippet.Title, snippet.Description, snippet.Categories)
-    }
-}
-
-func readFromFile(urls chan *Doc) {
+func readFromFile(urls chan<- *Doc) {
     file, err := os.Open("50.jsonl")
     if err != nil {
         log.Fatal(err)
@@ -201,20 +47,165 @@ func readFromFile(urls chan *Doc) {
     close(urls)
 }
 
+func getSnippet(wg *sync.WaitGroup, urls <-chan *Doc, snippets chan<- *Doc) { 
+    defer wg.Done()
+    for {
+        doc, ok := <- urls
+        if !ok {
+            return
+        }
+        client := &http.Client {
+            Timeout: 30 * time.Second,
+        }
+        resp, err := client.Get(doc.Url)
+
+        // if err != nil {
+        //     log.Println(err)
+        //     continue
+        // }
+        defer resp.Body.Close()
+
+        switch resp.StatusCode {
+        case 404:
+            log.Println(err)
+            continue
+        case 503:
+            log.Println(err)
+            continue
+        }
+        extract(resp.Body, doc)
+        
+        snippets <- doc
+    }
+}
+
+func extract(resp io.Reader, doc *Doc) {
+    z := html.NewTokenizer(resp)
+    titleFound := false
+    for {
+        tt := z.Next()
+        switch tt {
+        case html.ErrorToken:
+            return
+        case html.StartTagToken, html.SelfClosingTagToken:
+            t := z.Token()
+            if t.Data == `body` {
+                return
+            }
+            if t.Data == "title" {
+                titleFound = true
+            }
+            if t.Data == "meta" {
+                desc, ok := extractMetaProperty(t, "description")
+                if ok {
+                    doc.Description = desc
+                }
+
+                ogTitle, ok := extractMetaProperty(t, "og:title")
+                if ok {
+                    doc.Title = ogTitle
+                }
+
+                ogDesc, ok := extractMetaProperty(t, "og:description")
+                if ok {
+                    doc.Description = ogDesc
+                }
+            }
+        case html.TextToken:
+            if titleFound {
+                t := z.Token()
+                doc.Title = t.Data
+                titleFound = false
+            }
+        }
+    }
+    return
+}
+
+func extractMetaProperty(t html.Token, prop string) (content string, ok bool) {
+    for _, attr := range t.Attr {
+        if attr.Key == "property" && attr.Val == prop {
+            ok = true
+        }
+
+        if attr.Key == "content" {
+            content = attr.Val
+        }
+    }
+    return
+}
+
+func selectCategory(wg *sync.WaitGroup, snippets <-chan *Doc) {
+    for {
+        doc, ok := <-snippets
+        if !ok {
+            for _, category := range categories {
+                close(category)
+            }
+            return
+        }
+        for _, category := range doc.Categories {
+            if _, ok := categories[category]; !ok {
+                categories[category] = make(chan []string)
+                wg.Add(1)
+                go writeToTSV(wg, category, categories[category])
+            }
+            categories[category] <- []string{doc.Url, doc.Title, doc.Description}
+        }
+    }
+}
+
+func writeToTSV(wg *sync.WaitGroup, category string, lines <-chan []string) {
+    file, err := os.Create(fmt.Sprintf("%s.tsv", category))
+    checkError("Cannot create file ", err)
+
+    writer := csv.NewWriter(file)
+    writer.Comma = '\t'
+    // defer writer.Flush()
+    defer wg.Done()
+    defer file.Close()
+
+    for {
+        line, ok := <- lines
+        if !ok {
+            return
+        }
+        writer.Write(line)
+        writer.Flush()
+        if err := writer.Error(); err != nil {
+            log.Fatal("Error writing tsv: ", err)
+        }
+    }
+}
+
+
+func checkError(message string, err error) {
+    if err != nil {
+        log.Fatal(message, err)
+    }
+}
+
+func printSnippet(snippets <-chan *Doc) {
+    for snippet := range snippets {
+        fmt.Println(snippet.Url, snippet.Title, snippet.Description, snippet.Categories)
+    }
+}
 
 func main() {
-    urls := make(chan *Doc)
+    var wg_snippets, wg_files sync.WaitGroup
+    urls := make(chan *Doc, 10)
     snippets := make(chan *Doc)
 
     go readFromFile(urls)
 
-    for i := 0; i < 5; i++ {
-        go getSnippet(urls, snippets)
+    for i := 0; i < 3; i++ {
+        wg_snippets.Add(1)
+        go getSnippet(&wg_snippets, urls, snippets)
     }
-    selectCategory(snippets)
 
-    // for _, wrttr := range categories {
-    //     go writeToTSV(wrttr.File, wrttr.Channel)
-    // }
-    // printSnippet(snippets)
+    go selectCategory(&wg_files, snippets)
+
+    wg_snippets.Wait()
+    close(snippets)
+    wg_files.Wait()
 }
